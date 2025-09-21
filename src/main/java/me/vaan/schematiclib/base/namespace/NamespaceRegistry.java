1dfc6dc695d999f4692c5864d2a80cfe6f090c93
Intybyte <vaan.testwork@gmail.com>
2025-09-21 19:25:41 +0200

    Correct default namespace handling

--- src/main/java/me/vaan/schematiclib/base/namespace/NamespaceRegistry.java
+++ src/main/java/me/vaan/schematiclib/base/namespace/NamespaceRegistry.java
@@ -8,17 +8,26 @@ import java.util.UUID;
 
 public class NamespaceRegistry {
     protected Map<String, NamespaceHandler> registry = new HashMap<>();
+    private final String defaultNamespace;
     protected final NamespaceHandler defaultHandler;
 
-    public NamespaceRegistry(NamespaceHandler defaultHandler) {
+    public NamespaceRegistry(String defaultNamespace, NamespaceHandler defaultHandler) {
+        this.defaultNamespace = defaultNamespace;
         this.defaultHandler = defaultHandler;
     }
 
     public void registerNamespaceHandler(String namespace, NamespaceHandler predicate) {
+        namespace = namespace.toLowerCase();
+        if (namespace.equalsIgnoreCase(defaultNamespace)) return;
         registry.put(namespace, predicate);
     }
 
     public NamespaceHandler getNamespaceHandler(String namespace) {
+        namespace = namespace.toLowerCase();
+        if (namespace.equalsIgnoreCase(defaultNamespace)) {
+            return defaultHandler;
+        }
+
         return registry.get(namespace);
     }
 
