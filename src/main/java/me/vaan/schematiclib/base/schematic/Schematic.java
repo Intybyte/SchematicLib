cdcdc33a812b99f094aff1f61daf0364ff5f523b
Intybyte <vaan.testwork@gmail.com>
2025-09-22 12:14:51 +0200

    Add max & min + moveOrigin stuff

--- src/main/java/me/vaan/schematiclib/base/schematic/Schematic.java
+++ src/main/java/me/vaan/schematiclib/base/schematic/Schematic.java
@@ -26,4 +26,61 @@ public interface Schematic extends Iterable<IBlock> {
     default Iterator<IBlock> iterator() {
         return positions().iterator();
     }
+
+    default ICoord getMin() {
+        List<IBlock> blocks = positions();
+        if (blocks.isEmpty()) return null;
+
+        int minX = blocks.get(0).x();
+        int minY = blocks.get(0).y();
+        int minZ = blocks.get(0).z();
+
+        for (int i = 1; i < blocks.size(); i++) {
+            IBlock b = blocks.get(i);
+            if (b.x() < minX) minX = b.x();
+            if (b.y() < minY) minY = b.y();
+            if (b.z() < minZ) minZ = b.z();
+        }
+
+        return new FileCoord(minX, minY, minZ);
+    }
+
+    default ICoord getMax() {
+        List<IBlock> blocks = positions();
+        if (blocks.isEmpty()) return null;
+
+        int maxX = blocks.get(0).x();
+        int maxY = blocks.get(0).y();
+        int maxZ = blocks.get(0).z();
+
+        for (int i = 1; i < blocks.size(); i++) {
+            IBlock b = blocks.get(i);
+            if (b.x() > maxX) maxX = b.x();
+            if (b.y() > maxY) maxY = b.y();
+            if (b.z() > maxZ) maxZ = b.z();
+        }
+
+        return new FileCoord(maxX, maxY, maxZ);
+    }
+
+    default Schematic moveOrigin(ICoord coord) {
+        return moveOrigin(coord.x(), coord.y(), coord.z());
+    }
+
+    default Schematic moveOrigin(int x, int y, int z) {
+        ICoord coord = this.getMin();
+        int diffX = coord.x() - x;
+        int diffY = coord.y() - y;
+        int diffZ = coord.z() - z;
+        ICoord difference = new FileCoord(diffX, diffY, diffZ);
+
+        List<IBlock> newSchematic = new ArrayList<>();
+        for (IBlock block : this) {
+            newSchematic.add(block.add(difference));
+        }
+
+        return new FileSchematic(
+            newSchematic
+        );
+    }
 }
